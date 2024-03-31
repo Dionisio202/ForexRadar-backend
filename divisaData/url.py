579eba1b7ade6e18e93014d3666539baf87e6570
Edison <solisedison@outlook.com>
2024-03-31 15:09:04 -0500

    start user

--- divisaData/url.py
+++ divisaData/url.py
@@ -1,7 +1,8 @@
 from django.urls import path 
 from . import views 
 urlpatterns=[
-    path('currentData/', views.DivisaAPIViews.as_view()),
+    path('currentData/', views.DivisaAPIView.as_view()),
     path('tableData/', views.TableData.as_view()),
-  
+    path('dataForex/', views.ForexDataView.as_view()),
+     path('test/', views.SupabaseAuthView.as_view())
 ]
--- divisaData/views.py
+++ divisaData/views.py
@@ -10,6 +10,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import datetime
+from rest_framework import status 
+from supabase_py import create_client
 
 class TableData(APIView):
     def get(self, request, format=None):
@@ -94,34 +96,103 @@ class TableData(APIView):
         finally:
             # Cerrar el navegador
             driver.quit()
-            
-class DivisaAPIViews(APIView):
+
+class DivisaAPIView(APIView):
     def get(self, request):
         try:
-            # URL de la página
-            url = "https://au.finance.yahoo.com/quote/AUDUSD%3DX"
+            # Obtener el parámetro 'divisas' de la solicitud GET
+            currency_pair = request.query_params.get('divisas')
+
+            # Normalizar el formato del par de divisas
+            normalized_currency_pair = currency_pair.replace("/", "")
+
+            # Construir la URL utilizando el nombre de la divisa normalizado
+            url = f"https://au.finance.yahoo.com/quote/{normalized_currency_pair}%3DX"
+
+            # Configuración del navegador
+            options = webdriver.ChromeOptions()
+            options.add_argument("--start-maximized")  # Maximizar la ventana del navegador
+            options.add_argument("--headless") 
+            driver = webdriver.Chrome(options=options)
 
             # Hacer la solicitud GET a la URL
-            response = requests.get(url)
-            if response.status_code == 200:
-                tiempo_espera = random.uniform(8, 11)
-                time.sleep(tiempo_espera)
-                soup = BeautifulSoup(response.text, 'html.parser')
+            driver.get(url)
+            #time.sleep(3)  
 
-                # Encontrar el elemento que contiene el valor de la divisa
-                divisa_element = soup.find('fin-streamer', {'data-symbol': 'AUDUSD=X'})
+            # Obtener el HTML de la página
+            html = driver.page_source
 
-                # Extraer el valor del atributo "value"
-                if divisa_element:
-                    valor = divisa_element['value']
-                    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-                    # Guardar el valor en la base de datos o en algún otro lugar
-                    # Aquí podrías guardar el valor en tu base de datos si es necesario
+            # Crear objeto BeautifulSoup para analizar el HTML
+            soup = BeautifulSoup(html, 'html.parser')
 
-                    return Response({"timestamp": timestamp, "valor": valor})
-                else:
-                    return Response({"message": "No se pudo encontrar el valor de la divisa"}, status=404)
+            # Encontrar el elemento que contiene el valor de la divisa
+            divisa_element = soup.find('fin-streamer', {'data-symbol': f'{normalized_currency_pair}=X'})
+
+            # Extraer el valor del atributo "value"
+            if divisa_element:
+                valor = divisa_element['value']
+                timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+                # Guardar el valor en la base de datos o en algún otro lugar
+                # Aquí podrías guardar el valor en tu base de datos si es necesario
+
+                return Response({"timestamp": timestamp, "valor": valor})
             else:
-                return Response({"message": "No se pudo acceder a la página"}, status=response.status_code)
+                return Response({"message": "No se pudo encontrar el valor de la divisa"}, status=404)
         except Exception as e:
             return Response({"message": str(e)}, status=500)
+        finally:
+            # Cerrar el navegador
+            driver.quit()
+
+class ForexDataView(APIView):
+    def get(self, request):
+        # Parámetros de la solicitud
+        symbol = request.query_params.get('divisas')
+        function = 'TIME_SERIES_DAILY'  # Función para obtener datos diarios
+        apikey = 'V01MN0FOTVCZ17D4'  # Reemplaza 'TU_CLAVE_API' con tu clave API de Alpha Vantage
+        
+        # Especificar el rango de fechas para obtener solo datos del año 2024
+        start_date = '2024-01-01'
+        end_date = '2024-12-31'
+        #outputsize = 'full'  # Esto especifica que queremos obtener todos los datos disponibles
+
+        # Construir la URL de la solicitud con el rango de fechas
+        url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={apikey}&datatype=json&startdate={start_date}&enddate={end_date}'
+
+        # Realizar la solicitud GET
+        response = requests.get(url)
+
+        # Procesar la respuesta
+        if response.status_code == 200:
+            data = response.json()
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Error al obtener los datos"}, status=response.status_code)
+
+
+class SupabaseAuthView(APIView):
+    def get(self, request):
+        # Obtener datos de la solicitud
+        email = request.query_params.get('email')
+        password = request.query_params.get('password')
+
+        if not email or not password:
+            return Response({'error': 'Debes proporcionar un correo electrónico y una contraseña'}, status=400)
+
+        # Configurar el cliente Supabase
+        supabase_url = 'https://dcvauwnbzpxhdgggpzsg.supabase.co'
+        supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjdmF1d25ienB4aGRnZ2dwenNnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTg1OTUwNCwiZXhwIjoyMDI3NDM1NTA0fQ.AMi6GxQIuajsWeL_WQLSFFsXAfOufTwIpaN4gJxb8G4'
+        client = create_client(supabase_url, supabase_key)
+
+        # Autenticar el usuario
+        auth_response = client.auth.sign_up(email, password)
+
+        # Verificar el resultado de la autenticación
+        if 'status' in auth_response and auth_response['status'] == 200:
+            # Autenticación exitosa
+            # Aquí puedes devolver el token de acceso u otra información relevante
+            return Response({'token': auth_response['access_token']})
+        else:
+            # Autenticación fallida
+            # Devolver un mensaje de error
+            return Response({'error': auth_response.get('error', 'Error desconocido')}, status=400)
